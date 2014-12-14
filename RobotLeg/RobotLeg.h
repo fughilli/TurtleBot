a4ddf0ab53892fd38c113ef9e5bd44263ec07987
Kevin Balke <kbalke@ucla.edu>
2014-12-14 01:27:06 -0800

    Started class definition for RobotLeg

--- RobotLeg/RobotLeg.h
+++ RobotLeg/RobotLeg.h
@@ -1,10 +1,13 @@
 #ifndef _ROBOT_LEG_H_
 #define _ROBOT_LEG_H_
 
+#include "../VectorLib/Vector.h"
+
 class RobotLeg
 {
 public:
-	void setFoot()
+	void setFoot(const Vector3d& endEffector);
+	void getEnvelope();
 };
 
 #endif // _ROBOT_LEG_H_
